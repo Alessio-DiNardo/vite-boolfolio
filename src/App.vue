543b117e83346a85624521d3da5963627148dc4f
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-09-03 17:05:52 +0200

    Add axios

--- src/App.vue
+++ src/App.vue
@@ -13,16 +13,15 @@ export default{
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-  <AppMain/>
+  <header>
+    <AppHeader/>
+  </header>
+  <main>
+    <AppMain/>
+  </main>
+  <footer>
+    <AppFooter/>
+  </footer>
 </template>
 
 <style lang="scss">
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,10 +2,37 @@
     <div>
         AppMain
     </div>
+    <div>
+        <p v-for="post in posts">
+            {{post.title}}
+        </p>
+    </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
-    name: 'AppMain'
+    name: 'AppMain',
+
+    data() {
+        return {
+            posts : [],
+            apiUrl : 'http://127.0.0.1:8000/api/posts'
+        }
+    },
+
+    methods: {
+        getPosts(){
+                axios.get(this.apiUrl, {
+                    params: {}
+                })
+                .then(function (response) {
+                    this.posts = response.data.results.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+        }
+    },
 }
 </script>
 <style lang="scss">
